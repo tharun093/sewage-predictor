791fd8a5170205cdb98ed73ee4cf68f967cf969b
Tharun <tharun.cs18@bitsathy.ac.in>
2020-06-03 20:30:36 +0530

    first commit

---
+++ model.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import pickle
+
+dataset = pd.read_csv('regression1.csv')
+
+x = dataset.iloc[:, :3]
+y = dataset.iloc[:, -1]
+
+#Splitting Training and Test Set
+#Since we have a very small dataset, we will train our model with all availabe data.
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+
+#Fitting model with trainig data
+regressor.fit(x, y)
+
+# Saving model to disk
+pickle.dump(regressor, open('model.pkl','wb'))
